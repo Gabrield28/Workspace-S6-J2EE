14014df6c9c851ee659fc76be0e2cf50662eace3
Gabiboo <gabriel-dagorne@hotmail.fr>
2021-02-12 13:24:17 +0100

    TP a finir.

---
+++ TPServletFilter/src/FacadeServlet.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class FacadeServlet
+ */
+@WebServlet("/FacadeServlet")
+public class FacadeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FacadeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        this.getServletContext().getRequestDispatcher("/connexion.html").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		
+		String login = request.getParameter("login");
+		//String password = request.getParameter("password");
+		
+		session.setAttribute("login", login);
+		//session.setAttribute("password", password);
+		
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WelcomeServlet.java");
+        dispatcher.forward(request, response);
+        
+		//doGet(request, response);
+	}
+
+}
